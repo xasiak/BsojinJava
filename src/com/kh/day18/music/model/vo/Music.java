caf1350446186471c1795849c667f9f1057c16a6
배소진 <xasiak0926@gmail.com>
2023-07-19 16:35:04 +0900

    [ADD] 23/07/19 음악관리 프로그램 실습 완료
    1. run - view - controller - dao 구조를 이용하여 음악관리 프로그램 실습 완료
    2. MVC패턴 적용 완료

---
+++ src/com/kh/day18/music/model/vo/Music.java
@@ -0,0 +1,33 @@
+package com.kh.day18.music.model.vo;
+
+public class Music {
+	private String title;
+	public Music(String title, String singer) {
+		super();
+		this.title = title;
+		this.singer = singer;
+	}
+	public Music() {
+		super();
+	}
+	private String singer;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSinger() {
+		return singer;
+	}
+	public void setSinger(String singer) {
+		this.singer = singer;
+	}
+	@Override
+	public String toString() {
+		return "음악 [제목=" + title + ", 가수명=" + singer + "]";
+	}
+	
+	
+}
